aa68f13ab13a360a96b3d60dbc78963874343191
Till Niese <info@tn-dev.com>
2020-01-12 12:35:48 +0100

    ✅ added test for deg <=> rad conversion,

--- tests/angle.cpp
+++ tests/angle.cpp
@@ -3,25 +3,25 @@
 
 // #include <tndev/geo/angle.hpp>
 
-// //
+//
 
-// TEST_CASE("deg to rad", "[single-file]") {
-//     using tndev::geo::angle_cast;
-//     using tndev::geo::degree;
-//     using tndev::geo::radian;
+TEST_CASE("deg to rad", "[single-file]") {
+    using tndev::geo::angle_cast;
+    using tndev::geo::degree;
+    using tndev::geo::radian;
 
-//     auto angRad0Deg = angle_cast<radian>(degree(0.));
-//     auto angRad45Deg = angle_cast<radian>(degree(45.));
-//     auto angRad90Deg = angle_cast<radian>(degree(90.));
-//     auto angRad180Deg = angle_cast<radian>(degree(180.));
-//     auto angRad360Deg = angle_cast<radian>(degree(360.));
+    auto angRad0Deg = angle_cast<radian>(degree(0.));
+    auto angRad45Deg = angle_cast<radian>(degree(45.));
+    auto angRad90Deg = angle_cast<radian>(degree(90.));
+    auto angRad180Deg = angle_cast<radian>(degree(180.));
+    auto angRad360Deg = angle_cast<radian>(degree(360.));
 
-//     REQUIRE(angRad0Deg.count() == Approx(0));
-//     REQUIRE(angRad45Deg.count() == Approx(0.785398));
-//     REQUIRE(angRad90Deg.count() == Approx(1.5708));
-//     REQUIRE(angRad180Deg.count() == Approx(3.14159));
-//     REQUIRE(angRad360Deg.count() == Approx(6.28318));
-// }
+    REQUIRE(angRad0Deg.count() == Approx(0));
+    REQUIRE(angRad45Deg.count() == Approx(0.785398));
+    REQUIRE(angRad90Deg.count() == Approx(1.5708));
+    REQUIRE(angRad180Deg.count() == Approx(3.14159));
+    REQUIRE(angRad360Deg.count() == Approx(6.28318));
+}
 
 // TEST_CASE("deg literal", "[single-file]") {
 //     using namespace tndev::geo_literals;
