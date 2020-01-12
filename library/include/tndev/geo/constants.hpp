39e2ceeccfdf841318f5e85f32b2cf63fb9320e8
Till Niese <info@tn-dev.com>
2020-01-12 12:08:12 +0100

    ✨ added new constants

--- library/include/tndev/geo/constants.hpp
+++ library/include/tndev/geo/constants.hpp
@@ -5,10 +5,14 @@ namespace tndev::geo {
 constexpr inline double kMinLatitude = -85.05112878;
 constexpr inline double kMaxLatitude = 85.05112878;
 constexpr inline double kHalfCircleDegree = 180.;
+constexpr inline double kFullCircleDegree = 360.;
+constexpr inline double kQuaterCircleDegree = 90.;
 constexpr inline double kMinLongitude = -kHalfCircleDegree;
 constexpr inline double kMaxLongitude = kHalfCircleDegree;
 
 constexpr inline double kPI =
     3.1415926535897932384626433832795028841971693993751058209749445923078164062;
+
+constexpr inline double kPI2 = kPI * 2.;
 
 } // namespace tndev::geo
