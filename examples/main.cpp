f46ad502c5ae8cf3c70a15aa43737f666d37fe43
Till Niese <info@tn-dev.com>
2020-05-05 18:55:39 +0200

    ⚗ just a commit

--- examples/main.cpp
+++ examples/main.cpp
@@ -2,6 +2,10 @@
 #include <tndev/geo/bearing.hpp>
 #include <tndev/geo/latlng.distance.hpp>
 #include <tndev/geo/latlng.hpp>
+#include <tndev/geo/point.hpp>
+#include <tndev/geo/projection/mercator.hpp>
+#include <tndev/geo/quadkey.hpp>
+#include <tndev/geo/tile.hpp>
 
 #include <iostream>
 
@@ -12,6 +16,10 @@ auto main() -> int {
     using tndev::geo::extent_cast;
     using tndev::geo::latlng_deg;
     using tndev::geo::meters;
+    using tndev::geo::point;
+    using tndev::geo::radian;
+    using tndev::geo::tile;
+    using tndev::geo::projection::mercator;
 
     auto deg90 = tndev::geo::degree(90.);
     auto deg40 = tndev::geo::degree(40.);
@@ -35,6 +43,29 @@ auto main() -> int {
     auto north = latlng_deg(47.692030, 9.186282);
     auto south = latlng_deg(47.691145, 9.186357);
     auto east = latlng_deg(47.691640, 9.186845);
+
+    auto centerTile = get_enclosing_tile<mercator>(center, 18);
+    std::cout << "qk: " << tile_to_quadkey(centerTile) << std::endl;
+
+    auto p1 = point<mercator>(center);
+    auto t = tile<mercator>();
+    auto t1 = tile<mercator>(1, 1, 2);
+    auto m = mercator(5, 5, 2, 512);
+    std::cout << "qk: " << tile_to_quadkey(t1) << std::endl;
+    auto t2 = tndev::geo::quadkey_to_tile<mercator>(tile_to_quadkey(t1));
+
+    auto p2 = point<mercator>(p1.projected(m), m);
+
+    std::cout << "rel   : " << p1.x() << " " << p1.y() << std::endl;
+    std::cout << "rel2  : " << p2.x() << " " << p2.y() << std::endl;
+    std::cout << "prj   : " << p1.projected(m).x << " " << p1.projected(m).y
+              << std::endl;
+    auto p1ll = p1.latlng();
+    std::cout << "ll    : " << p1ll.lat().count() << " " << p1ll.lng().count()
+              << std::endl;
+    std::cout << "orgll : " << radian(center.lat()).count() << " "
+              << radian(center.lng()).count() << std::endl;
+
     auto west = latlng_deg(47.691593, 9.185574);
 
     std::cout << "bearing center -> north  "
